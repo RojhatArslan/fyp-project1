8b2c8fac20e68ce137b783ed6a09125516765b9f
Roj A <roja@MacBook-Air-2.local>
2026-01-28 14:40:45 +0000

    Completion of linear

--- src/models.py
+++ src/models.py
@@ -1,12 +1,14 @@
 import pandas as pd 
-from pathlid import Path
+from pathlib import Path
 
 
 from sklearn.linear_model import LinearRegression
-from sklearn.metrics import mean_squared_error import mean_absolute_error, mean_absolute_percentage_error, r2_score
+from sklearn.metrics import mean_squared_error,mean_absolute_error, mean_absolute_percentage_error, r2_score
 
 
-def train_linear_regression(data_path: Path)
+
+def train_linear_regression(data_path: Path):
+
     df = pd.read_csv(data_path)
     df = df.sort_values("ds").reset_index(drop=True)
 
@@ -14,4 +16,24 @@ def train_linear_regression(data_path: Path)
     y = df["bookings"]
 
 
+    split_idx = int(len(df) * 0.8)
+
+    X_train, X_test = X.iloc[:split_idx], X.iloc[split_idx:]
+    y_train, y_test = y.iloc[:split_idx], y.iloc[split_idx:]
+
+    model = LinearRegression()
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+        
+    mae = mean_absolute_error(y_test, y_pred)
+    rmse = mean_squared_error(y_test, y_pred,squared= False)
+    r2 = r2_score(y_test, y_pred)
+
+    print("Linear Regression Model Performance:")
+    print(f"MAE: {mae:.2f}")
+    print(f"RMSE: {rmse:.2f}")
+    print(f"R2: {r2:.2f}")
+
+    return mae,rmse,r2
     
